c72a992950da10dc95b6fc2b15d2d325aec26d45
aniket777984 <aniket777984@gmail.com>
2023-12-04 14:12:54 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import SignUp from "./pages/SignUp";
 
